38478d18c0909dd5fe090036790fde94e9e9de56
SebastianAlfonsoMartinez <sebastianalfonso94@gmail.com>
2023-10-18 20:25:36 -0500

    Modificacion en main para ingreso de valores para operaciones matematicas por consola

--- src/com/sam/arrayList/Main.java
+++ src/com/sam/arrayList/Main.java
@@ -6,24 +6,30 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
         System.out.println("");
         Suma suma = new Suma();
-        System.out.println(suma.operaciones(5, 7));
+        System.out.println("Ingrese valores para la suma: ");
+        System.out.println("resultado suma: " + suma.operaciones(scanner.nextInt(), scanner.nextInt()));
         Resta resta = new Resta();
-        System.out.println(resta.operaciones(75, 25));
+        System.out.println("Ingrese valores para la resta: ");
+        System.out.println("resultado resta: " + resta.operaciones(scanner.nextInt(), scanner.nextInt()));
         Multiplicacion multiplicacion = new Multiplicacion();
-        System.out.println(multiplicacion.operaciones(8, 9));
+        System.out.println("Ingrese valores para la multiplicacion: ");
+        System.out.println("resultado multiplicacion: " + multiplicacion.operaciones(scanner.nextInt(), scanner.nextInt()));
         Division division = new Division();
-        System.out.println(division.operaciones(20, 4));
+        System.out.println("Ingrese valores para la division: ");
+        System.out.println("resultado division: " + division.operaciones(scanner.nextInt(), scanner.nextInt()));
         Modulo modulo = new Modulo();
-        System.out.println(modulo.operaciones(10, 2));
+        System.out.println("Ingrese valores para la residuo: ");
+        System.out.println("resultado residuo: " + modulo.operaciones(scanner.nextInt(), scanner.nextInt()));
 
 
-        Scanner lecturaMatriz = new Scanner(System.in);
+
         System.out.println("Ingrese el tamano de su matriz 1");
-        Integer num1 = lecturaMatriz.nextInt();
+        Integer num1 = scanner.nextInt();
         System.out.println("Ingrese el tamano de su matriz 2");
-        Integer num2 = lecturaMatriz.nextInt();
+        Integer num2 = scanner.nextInt();
 
 
         int[] matrizUno = new int[num1];
@@ -33,12 +39,12 @@ public class Main {
         //1.llenando  la primera matriz
         for (int i = 0; i < matrizUno.length; i++) {
             System.out.println("Digita el numero en la posicion [" + i + "]");
-            matrizUno[i] = lecturaMatriz.nextInt();
+            matrizUno[i] = scanner.nextInt();
         }
         //2.llenando  la segunda matriz
         for (int i = 0; i < matrizDos.length; i++) {
             System.out.println("Digita el numero en la posicion [" + i + "]");
-            matrizDos[i] = lecturaMatriz.nextInt();
+            matrizDos[i] = scanner.nextInt();
         }
 
         int[] resultadoMatriz = new int[matrizUno.length];
